fd70da809359dcaaa3a8b925a1bbca831389851b
jirov <jirovgrystnov@gmail.com>
2026-01-28 10:48:19 +0700

    updateundangan

--- application/views/home_undian.php
+++ application/views/home_undian.php
@@ -71,10 +71,7 @@
           </div>
           
           <?php 
-            // $queryhadiah = "SELECT * FROM peserta WHERE no_peg = '".$this->session->userdata('username')."' and is_checkin = 0";
-            
-            $querypeserta = "SELECT * FROM peserta WHERE no_peg IN ('KW16004','KW21012')";
-            
+            $queryhadiah = "SELECT * FROM peserta WHERE no_peg = '".$this->session->userdata('username')."'";
             $rdt = $this->db_undian->query($querypeserta)->num_rows();
             if($rdt != 0){
                 $data = $this->db_undian->query($querypeserta)->result();
@@ -84,12 +81,11 @@
           <div class="col-12 col-sm-12 col-lg-12">
 
 
-
           <div class="card card-bg-img bg-img" onclick="undangansic('<?php echo $nopeg?>')" style="background-image: url('<?php echo base_url()?>assets/img/bg-img/undangangathering.jpeg')">
             <div class="card-body p-4 direction-rtl" style="height: 180px;">
               <h6 class="display-3 mb-4 fw-semibold"></h6>
             </div>
-            <a class="btn btn-danger">Generate QR Code</a>
+            <a class="btn btn-danger">UNDANGAN DIGITAL FAMILY GATHERING</a>
           </div>
           
           </div>
@@ -108,7 +104,7 @@
               <div class="card-body p-4 direction-rtl" style="height: 180px;">
                 <h6 class="display-3 mb-4 fw-semibold"></h6>
               </div>
-              <a class="btn btn-primary">Generate QR Code</a>
+              <a class="btn btn-primary">TUKARKAN HADIAH</a>
             </div>
           </div>
           <?php 
@@ -390,9 +386,6 @@ function tukarhadiah(){
     window.location.href='<?php echo base_url(); ?>index.php/welcome/tukarhadiah';
 }
 
-// function undangansic(){
-//     window.location.href='<?php echo base_url(); ?>index.php/welcome/undangansic';
-// }
 
 function undangansic(nopeg){
     window.open('https://undian2026.sic.co.id/undangan/?no_undian='+nopeg);
